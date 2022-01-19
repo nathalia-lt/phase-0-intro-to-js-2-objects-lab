941e9f86db27e21e46b79134bf0d13ac77258557
nathalia <nathaliatroina@gmail.com>
2022-01-18 21:21:51 -0500

    commit tree 2b100638c2f57b3059050cddc559e72ad0d4ed76

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-// Write your solution in this file!
 
-const employee = {
-    name: "",
-    streetAddress: "",
-}
+const employee = { name:"", addressStreet: ""}
+
+// u define an object w/ curly bracket, which obj has a key with
+//value (an obj without any key is an empty obj ex: i = {})
+
+//employee.name
+// if you want to access a key's value
+//every obj is mutable, if you change de value of a key,
+//it means that the value mutates.
+//to not mutate you need to create a copy.
 
 function updateEmployeeWithKeyAndValue(employee, key, value){
-    return Object.assign({}, employee, {[key]:value})
+    const newEmployee = {...employee} //does not mutate
+    newEmployee[key] = value
+    //const newEmployee = {key:'value'}
+    return newEmployee
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
-    employee[key] = value;
-    return employee;
-}
-
-function deleteFromEmployeeByKey(employee, key) {
-    const newObject = Object.assign({}, employee);
-    delete newObject[key];
-    return newObject;
-}
-
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key];
+ function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
     return employee
-}
+ } 
+
+ function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee
+ }
+
+ function destructivelyDeleteFromEmployeeByKey(employee, key){
+     delete employee[key]
+     return employee 
+ }
